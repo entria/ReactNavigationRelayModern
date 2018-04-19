c558e66a5164b6dc6566f0884176b88176bb6f4f
jaburcodes <guilhermejabur@outlook.com>
2018-04-19 19:43:51 -0300

    add UserCreate to navigator

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import { StackNavigator } from 'react-navigation';
 
+import UserCreate from './UserCreate';
 import UserList from './UserList';
 import UserDetail from './UserDetail';
 
 const RelayApp = StackNavigator(
   {
+    UserCreate: { screen: UserCreate },
     UserList: { screen: UserList },
     UserDetail: { screen: UserDetail },
   },
   {
-    initialRouteName: 'UserList',
+    initialRouteName: 'UserCreate',
   },
 );
 
